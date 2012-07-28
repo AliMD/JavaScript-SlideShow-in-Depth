f6cfac3fe3eebc47dda567f780b5fc20fca88e79
alimd <ali@mihandoost.com>
2012-07-29 04:11:35 +0430

    02 JS Basic 2

---
+++ 02 JS Basic 2/slideshow.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+	var slideshow = document.getElementsByClassName('slideshow').item(0);
+	var lists = slideshow.getElementsByClassName('btns').item(0).getElementsByTagName('li');
+	for (var i=0; i<lists.length; i++) {
+		tmp = function(j){
+			lists.item(j).onclick=function(){
+				alert(j);
+			}
+		}
+		tmp(i);
+	}
+}
