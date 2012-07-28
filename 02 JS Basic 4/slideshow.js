f2dddde02f65f0c0f943f9b3e9fd802fb2c4c576
alimd <ali@mihandoost.com>
2012-07-29 04:11:50 +0430

    02 JS Basic 4

---
+++ 02 JS Basic 4/slideshow.js
@@ -0,0 +1,17 @@
+window.onload=function(){
+	var slideshow = document.getElementsByClassName('slideshow').item(0);
+
+	var train = slideshow.getElementsByClassName('train').item(0);
+	go2slide = function (n) {
+		train.style.left=(-450*n)+'px';
+	}
+
+	var lists = slideshow.getElementsByClassName('btns').item(0).getElementsByTagName('li');
+	for (var i=0; i<lists.length; i++) {
+		(function(j){
+			lists.item(j).onclick=function(){
+				go2slide(j);
+			}
+		})(i);
+	}
+}
