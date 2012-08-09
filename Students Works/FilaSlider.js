6e6815592344b4549f1776648639287f626ac8e9
Mohammad Reza Shariatzadeh <Mrshcom@gmail.com>
2012-08-10 02:41:17 +0430

    Create Functions for show/hide information of Pics in Slider

--- Students Works/FilaSlider.js
+++ Students Works/FilaSlider.js
@@ -3,7 +3,8 @@ window.onload = function(){
 
 	var slideshow = document.getElementsByClassName('slideshow').item(0);
 	var train = slideshow.getElementsByClassName('case-train').item(0).getElementsByClassName('train').item(0);
-	var img = train.getElementsByTagName('div'); 
+	var img = train.getElementsByClassName('img');
+	//var info = img.getElementsByClassName('info');
 	
 	var pre = slideshow.getElementsByClassName('pre').item(0); 
 	var next = slideshow.getElementsByClassName('next').item(0); 
@@ -11,10 +12,19 @@ window.onload = function(){
 	
 	slider = function(n){
 		if(n>(img.length-1)) n=0;
-		if(n<0) n = (img.length-1);
+		if(n<0) n = (img.length-2);
 		train.style.left = -(n*230) + 'px';
 		currentslide = n;
-		//alert();
+	}
+	
+	show_info = function(s){
+		img.item(s).getElementsByClassName('info').item(0).style.bottom = 0 + 'px';
+		img.item(s).getElementsByClassName('info').item(0).style.WebkitTransition = "all 400ms";
+	}
+	
+	hide_info = function(s){
+		img.item(s).getElementsByClassName('info').item(0).style.bottom = -80 + 'px';
+		img.item(s).getElementsByClassName('info').item(0).style.WebkitTransition = "all 1000ms";
 	}
 	
 	nextslide = function(){
@@ -23,6 +33,20 @@ window.onload = function(){
 	
 	preslide = function(){
 		slider(currentslide-1);
+	}
+	
+	for(var i=0; i<9 ; i++){
+		(function(j){
+			img.item(j).onmouseover = function(){
+				show_info(j);	
+			}
+		})(i);
+		
+		(function(j){
+			img.item(j).onmouseout = function(){
+				hide_info(j);	
+			}
+		})(i);
 	}
 	
 	next.onclick = nextslide;
