391a92c62e477820749b966592555b31d52ec67c
Ali mihandoost <ali@mihandoost.com>
2012-08-09 15:13:09 +0330

    increase width when opened : 1*450 + 4*50

--- 08 AccSlide style/slideshow.js
+++ 08 AccSlide style/slideshow.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-	$('div.slideshow div.train > div').css('width',(450/5)+'px');
+	$('div.slideshow div.train > div').css('width',Math.ceil(650/5)+'px');
 })
 
---
+++ 09 AccSlide JS/slideshow.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$('div.slideshow div.train > div').css('width',(450/5)+'px');
+})
+
