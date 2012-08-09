0f08b80987ff4dac5db6a7546c21d72dd6e5a7f1
Mohammad Reza Shariatzadeh <Mrshcom@gmail.com>
2012-08-09 14:55:46 +0430

    Rename file to FilaSlider

---
+++ Students Works/FilaSlider.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Fila Slider</title>
+<link type="text/css" rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+	<div class="container">
+    	<div class="slideshow">
+        	<div class="train">
+            	<div style="background-image:url(../images/pic1.jpg);"></div>
+            	<div style="background-image:url(../images/pic2.jpg);"></div>
+            	<div style="background-image:url(../images/pic3.jpg);"></div>
+            	<div style="background-image:url(../images/pic2.jpg);"></div>
+            	<div style="background-image:url(../images/pic3.jpg);"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
