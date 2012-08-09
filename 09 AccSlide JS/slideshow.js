9feeb79716721af0bd5ea9a69d616008fb7fe4eb
Ali mihandoost <ali@mihandoost.com>
2012-08-09 15:32:36 +0330

    add js

--- 09 AccSlide JS/slideshow.js
+++ 09 AccSlide JS/slideshow.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
-	$('div.slideshow div.train > div').css('width',(450/5)+'px');
+	var slidesWidth = Math.ceil(650/5);
+
+	slides = $('div.slideshow div.train > div');
+	slides.css({width:slidesWidth+'px'});
+
+	slides.mouseover(function(){
+		slides.animate({width:'50px'},500);
+		$(this).animate({width:'450px'},500);
+	});
+	$('div.slideshow').mouseout(function(){
+		slides.animate({width:slidesWidth+'px'},500);
+	});
 })
 
