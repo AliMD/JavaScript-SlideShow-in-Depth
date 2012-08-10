93616499235a570dbafd94078e3aadfd6d68258e
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-08-10 23:49:31 +0430

    change code and update something

--- Students Works/Home work/Acc Slide/Accslide.js
+++ Students Works/Home work/Acc Slide/Accslide.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-	slides= $('div.slideshow .train > div');
-	var widths= Math.ceil(1160/5);
+	slides= $('div.container div.slideshow div.train > div');
+	var widths= Math.ceil(870/7);
 
 	slides.css({width:widths+'px'});
 	slides.last().css({'padding-right':'500px'});
 
 	slides.mouseover(function(){
 		slides.animate({width:'50px'},500);
-		$(this).animate({width:'960px'},500);
+		$(this).animate({width:'570px'},500);
 	})
 	slides.mouseout(function(){
 		slides.animate({width:widths+'px'},500);
--- Students Works/Home work/Acc Slide/index.php
+++ Students Works/Home work/Acc Slide/index.php
@@ -3,9 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Acc Slideshow</title>
-	<link rel="stylesheet" type="text/css" href="reset.css" />
+	<link rel="stylesheet" type="text/css" href="../reset.css" />
 	<link rel="stylesheet" type="text/css" href="accslide.css" />
-	<script type="text/javascript" src="zepto.js"></script>
+	<script type="text/javascript" src="../zepto.js"></script>
 	<script type="text/javascript" src="Accslide.js"></script>
 </head>
 <body>
@@ -13,12 +13,12 @@
 		<div class="slideshow">
 			<div class="train">
 				<?php
-					$path= './images/';
+					$path= '../images/';
 					$files= scandir($path);
 					foreach ($files as $file) {
 						$exp= explode('.',$file);
 						$type= end($exp);
-						if ($type == 'png'){
+						if ($type == 'jpg'){
 							echo "<div style=\"background-image:url('$path/$file');\"></div>";
 						}
 					}
