ba0b7d5f7198ff57e547a8cb7aee0535d18c1c4b
Mohammad Reza Shariatzadeh <Mrshcom@gmail.com>
2012-08-10 16:28:42 +0430

    Optimize codes and Remove Extra Codes

--- Students Works/FilaSlider.js
+++ Students Works/FilaSlider.js
@@ -5,7 +5,6 @@ window.onload = function(){
 	var case_train = slideshow.getElementsByClassName('case-train').item(0);
 	var train = case_train.getElementsByClassName('train').item(0);
 	var img = train.getElementsByClassName('img');
-	//var info = img.getElementsByClassName('info');
 	
 	var pre = slideshow.getElementsByClassName('pre').item(0); 
 	var next = slideshow.getElementsByClassName('next').item(0); 
@@ -16,16 +15,6 @@ window.onload = function(){
 		if(n<0) n = (img.length-3);
 		train.style.left = -(n*233) + 'px';
 		currentslide = n;
-	}
-	
-	show_info = function(s){
-		img.item(s).getElementsByClassName('info').item(0).style.bottom = 0 + 'px';
-		img.item(s).getElementsByClassName('info').item(0).style.WebkitTransition = "all 400ms";
-	}
-	
-	hide_info = function(s){
-		img.item(s).getElementsByClassName('info').item(0).style.bottom = -80 + 'px';
-		img.item(s).getElementsByClassName('info').item(0).style.WebkitTransition = "all 1000ms";
 	}
 	
 	nextslide = function(){
@@ -39,27 +28,13 @@ window.onload = function(){
 	var intervalName = false;
 	(startSliding = function(){
 		if(intervalName) return; 
-		intervalName = setInterval(nextslide,1000);
+		intervalName = setInterval(nextslide,3000);
 	})();
 	
 	stopSliding = function(){
 		if(!intervalName) return;
 		clearInterval(intervalName);
 		intervalName = false;
-	}
-	
-	for(var i=0; i<9 ; i++){
-		(function(j){
-			img.item(j).onmouseover = function(){
-				show_info(j);	
-			}
-		})(i);
-		
-		(function(j){
-			img.item(j).onmouseout = function(){
-				hide_info(j);	
-			}
-		})(i);
 	}
 	
 	next.onclick = nextslide;
