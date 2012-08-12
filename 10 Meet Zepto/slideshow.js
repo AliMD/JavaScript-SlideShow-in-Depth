9368fcf65390c6b7d09eba1daad04b6d656fad10
Ali mihandoost <ali@mihandoost.com>
2012-08-12 15:56:14 +0330

    switch to zepto !

--- 10 Meet Zepto/slideshow.js
+++ 10 Meet Zepto/slideshow.js
@@ -1,45 +1,41 @@
-window.onload=function(){
-	var slideshow = document.getElementsByClassName('slideshow').item(0),
-		train = slideshow.getElementsByClassName('train').item(0),
-		lists = slideshow.getElementsByClassName('btns').item(0).getElementsByTagName('li'),
-		currentSlide = 0;
+$(function(){ // document ready
+	var slideshow     = $('div.slideshow'),
+		train         = $('div.slideshow .train'),
+		lists         = $('div.slideshow ul.btns li'),
+		currentSlide  = 0;
 
 	(go2slide = function (n) {
 		if(n>lists.length-1) n=0;
 		if(n<0) n=lists.length-1;
-		train.style.left=(-450*n)+'px';
-		lists.item(currentSlide).className = '';
-		lists.item(n).className = 'active';
+		train.css({left: (-450*n)+'px' });
+		lists.eq(currentSlide).removeClass('active');
+		lists.eq(n).addClass('active');
 		currentSlide=n;
-	})(0); // set active of first li
+	})(0); 
 
-	nextSlide = function(){
+	lists.click(function(){
+		go2slide($(this).index());
+	});
+
+	$('div.slideshow .next').click(nextSlide=function(){
 		go2slide(currentSlide+1);
-	}
+	});
 
-	prvSlide = function(){
+	$('div.slideshow .previous').click(function(){
 		go2slide(currentSlide-1);
-	}
+	});
 
 	var autoPlayIv = false;
-	(autoPlayStart = slideshow.onmouseout = function(){
+	(autoPlayStart = function(){
 		if(autoPlayIv) return;
 		autoPlayIv = setInterval(nextSlide, 5000);
-	})(); // run auto play
+	})();
 
-	autoPlayStop = slideshow.onmouseover = function(){
+	autoPlayStop = function(){
 		clearInterval(autoPlayIv);
 		autoPlayIv = false;
 	}
 
-	slideshow.getElementsByClassName('next').item(0).onclick=nextSlide;
-	slideshow.getElementsByClassName('previous').item(0).onclick=prvSlide;
-
-	for (var i=0; i<lists.length; i++) {
-		(function(j){
-			lists.item(j).onclick=function(){
-				go2slide(j);
-			}
-		})(i);
-	}
-}
+	slideshow.mouseover(autoPlayStop);
+	slideshow.mouseout(autoPlayStart);
+});
