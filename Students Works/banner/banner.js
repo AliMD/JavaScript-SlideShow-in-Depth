5b186ba8bedc56d4f67a35daf3544975540c06d0
mehrad <mehrad.kavian@gmail.com>
2012-08-13 14:23:48 +0430

    set autoplay

--- Students Works/banner/banner.js
+++ Students Works/banner/banner.js
@@ -1,5 +1,6 @@
 $(function(){
-	var train=$('div.slideshow div.train'),
+	var slideshow=$('div.slideshow'),
+	train=$('div.slideshow div.train'),
 	btns=$('div.slideshow ul.btns li'),
 	currentSlide=0,picwidth=1000;
 	(go2Slide=function(n){
@@ -15,15 +16,19 @@ $(function(){
 	btns.click(function(){
 		go2Slide($(this).index());
 	});
-	$('div.slideshow div.next').click(function(){
+	$('div.slideshow div.next').click(next=function(){
 		go2Slide(currentSlide+1);
 	});
 	$('div.slideshow div.previous').click(function(){
 		go2Slide(currentSlide-1);
 	});
 
-	
-	setInterval(nextSlide,5000);
+	var autoPlayIv=false;
+	(autoPlayStart = function(){
+		if(autoPlayIv) return;
+		setInterval(nextSlide,5000);
+	})
+
 };
 
 
