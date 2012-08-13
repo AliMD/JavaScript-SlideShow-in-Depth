d47a27ccbef49992ddcfacfa461cf93c72db6c68
Mohammad Reza Shariatzadeh <Mrshcom@gmail.com>
2012-08-13 12:31:05 +0430

    Add js Function

--- Students Works/Accordion Slider/AccSlider_Mr.js
+++ Students Works/Accordion Slider/AccSlider_Mr.js
@@ -1,4 +1,18 @@
 
 $(document).ready(function() {
-    
+    var slidesWidth = Math.ceil(825/4);
+	
+	slides = $('div.slideshow div.train > div');
+	slides.css({width:slidesWidth + 'px'});
+	slides.last().css({'padding-right':'200px'});
+	
+	slides.mouseover(function(){
+		slides.animate({width:'115px'},500);
+		$(this).animate({width:'480px'},500);
+	});
+	
+	$('div.slideshow').mouseout(function(){
+		slides.animate({width:slidesWidth + 'px'});	
+	});
+	
 });
