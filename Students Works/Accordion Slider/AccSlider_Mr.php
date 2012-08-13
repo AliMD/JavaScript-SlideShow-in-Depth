06809c1b322fc0782eef3d03ac2fdf57be0eed74
Mohammad Reza Shariatzadeh <Mrshcom@gmail.com>
2012-08-13 13:55:24 +0430

    Add div.header and etc tag to PHP file

--- Students Works/Accordion Slider/AccSlider_Mr.php
+++ Students Works/Accordion Slider/AccSlider_Mr.php
@@ -27,7 +27,22 @@
 					}
 					
                 ?><?php */?>
-                <div style="background-image:url(../../images/AccSlider_Mr/AccSlider1.jpg)"></div>
+                <div style="background-image:url(../../images/AccSlider_Mr/AccSlider1.jpg)">
+                	<div class="header">Meiklejohn</div>
+                    <div class="bg-slide"></div>
+                    <div class="description">
+                    	<h2>Meiklejohn</h2>
+                        <p>
+                        	Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                            ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                            nulla pariatur.
+                        </p>
+                        <a href="#">more →</a>
+                    </div>
+                </div>
                 <div style="background-image:url(../../images/AccSlider_Mr/AccSlider2.jpg)"></div>
                 <div style="background-image:url(../../images/AccSlider_Mr/AccSlider3.jpg)"></div>
                 <div style="background-image:url(../../images/AccSlider_Mr/AccSlider4.jpg)"></div>
