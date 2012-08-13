c09ae9197f3e8d91613f4875245829edcc526110
Mohammad Reza Shariatzadeh <Mrshcom@gmail.com>
2012-08-13 11:51:34 +0430

    Add AccSlider.php file to new Project(Accordion Slider)

---
+++ Students Works/Accordion Slider/AccSlider_Mr.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Accordion Slider</title>
+<link type="text/css" rel="stylesheet" href="../../reset.css" />
+<link type="text/css" rel="stylesheet" href="AccSlider_Mr.css" />
+</head>
+
+<body>
+	<div class="container">
+    	<div class="slideshow">
+        	<div class="train">
+           <?php /*?> 	<?php
+					$images_path = "../../images/AccSlider_Mr";
+                	$images = scandir($images_path);
+					
+					foreach($images as $img){
+						$pic_arr = explode('.',$img);
+						$pic_type = strtolower(end($pic_arr));
+						if($pic_type == "jpg"){
+								echo "hi";
+							}
+							
+					}
+					
+                ?><?php */?>
+                <div style="background-image:url(../../images/AccSlider_Mr/AccSlider1.jpg)"></div>
+                <div style="background-image:url(../../images/AccSlider_Mr/AccSlider2.jpg)"></div>
+                <div style="background-image:url(../../images/AccSlider_Mr/AccSlider3.jpg)"></div>
+                <div style="background-image:url(../../images/AccSlider_Mr/AccSlider4.jpg)"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
