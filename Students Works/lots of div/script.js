5f9c5d0c827997c1272f17b564c32e86e1a50741
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-08-15 03:37:11 +0430

    Chage url to work here

---
+++ Students Works/lots of div/script.js
@@ -0,0 +1,46 @@
+// Javascript
+
+$(function(){
+	var part = $('div.slideshow div.part');
+	var li = $('div.menu ul li');
+	//set background position of each part
+
+	part.each(function(i){
+		if(i<4){
+			$(this).css({backgroundPosition:(i)*-112.5+"px 0px"});
+		}
+		else
+		if(i<8){
+			$(this).css({backgroundPosition:(i)*-112.5+"px -100px"});
+		}
+		else{
+			$(this).css({backgroundPosition:(i)*-112.5+"px -200px"});
+		}
+	});
+
+	// changeBackground 
+
+	cbg = function(n){
+		part.each(function(i){
+			$(this).animate({backgroundImage:'url(\'../../images/pic'+(n+1)+'.jpg\')'},i*200);
+		})
+	};
+	
+	//Set menu JS
+	var cr = 0
+	li.each(function(i){
+		$(this).click(function(){
+			gotopic(i);
+		});
+	});
+
+	gotopic = function(n){
+		li.eq(cr).removeClass('active');
+		li.eq(n).addClass('active');
+		cbg(n);
+		cr = n;
+	}
+	gotopic(0);
+
+
+});
