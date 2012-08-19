9418f1e6d6a43927e7fbdd5d291e3af38bcf2c8d
mahlad <mahlad365@gmail.com>
2012-08-19 16:23:20 +0430

    This is the first version of this slideshow.
    https://github.com/mahlad/mahlad-PartofImg-Slideshow

---
+++ Students Works/mahlad-PartofImg-Slideshow/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Part of Image Slideshow</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script type="text/javascript" src="script/jquery.js"></script>
+	<script type="text/javascript" src="script/partofImg.js"></script>
+</head>
+<body>
+	<div class="partSl">
+		<div class="imgSl">
+			<?php 
+				$path='images';
+				$img_arr=scandir($path);
+				$all_type='jpg,png,gif,jpeg';
+				$all_type=explode(',',$all_type);
+				foreach ($img_arr as $each_img) {
+					$img_type=explode('.',$each_img);
+					$end_type=strtolower(end($img_type));
+					if(in_array($end_type , $all_type)){
+
+						echo "
+							<div>
+								<img src='$path/$each_img' alt='$each_img'>
+								<span>ice creame</span>
+							</div>
+							";
+					}
+				}
+			 ?>
+			 
+			
+		</div>
+	</div>	
+</body>
+</html>
---
+++ Students Works/mahlad-PartofImg-Slideshow/script/partofImg.js
@@ -0,0 +1,32 @@
+$(function () {
+	var slideshow=$("div.partSl"),
+		listImg=$("div.partSl div.imgSl div"),
+		totalImg=$("div.partSl div.imgSl div").length;
+	(position=function(m){
+		for(var n=1;n<=m;n++)
+		{
+			if(n==1)
+				$("div.partSl div:eq("+n+")").animate({left:'0px'},2000);
+			else
+			$("div.partSl div:eq("+n+")").animate({left:((n-1)*60)+440+'px'},2000);
+
+		}
+	})(totalImg);
+	
+	listImg.click(function(){
+		var ind=$("div.partSl div.imgSl div").index(this);
+		//alert(listImg.index());
+		for(var i=0;i<ind;i++){
+			$("div.partSl div.imgSl div:eq("+i+")").animate({left:60*i+'px'},1000);
+
+		}
+		$(this).animate({left:60*ind+'px'},1000);
+		for(var j=ind+1;j<totalImg;j++){
+			$("div.partSl div.imgSl div:eq("+j+")").stop();
+			$("div.partSl div.imgSl div:eq("+j+")").animate({left:(60*j)+440+'px'},1000);
+
+		}
+	});
+	
+	
+});
