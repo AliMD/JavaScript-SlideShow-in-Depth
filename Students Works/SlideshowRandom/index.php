82dd5ce8dbfc058e2a9e34bd73a894548213319a
mahlad <mahlad365@gmail.com>
2012-08-21 02:00:25 +0430

    change the period of css

--- Students Works/SlideshowRandom/index.php
+++ Students Works/SlideshowRandom/index.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Js SlideShow Tow</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script type="text/javascript" src="../../zepto.js" ></script>
 	<script type="text/javascript" src="script/SlideTow.js"></script>
 </head>
