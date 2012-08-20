bafabf6564bd6e79e1fafd129e7a7eb97d03d1e0
mahlad <mahlad365@gmail.com>
2012-08-21 01:48:41 +0430

    correct the cods and use main jquery

--- Students Works/SlideshowRandom/index.php
+++ Students Works/SlideshowRandom/index.php
@@ -3,19 +3,17 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Js SlideShow Tow</title>
-	<link type="text/css" rel="stylesheet"  href="css/style.css">
-	<link type="text/css" rel="stylesheet" href="css/reset.css">
-	
-	<script type="text/javascript" src="script/zepto.js" ></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<script type="text/javascript" src="../../zepto.js" ></script>
 	<script type="text/javascript" src="script/SlideTow.js"></script>
 </head>
 <body>
 
 	<div class="container">
-		
 		<div class="slTow">
 			<?php
-				$path='images';
+				$path='./images';
 				$img_arr=scandir($path);
 				$img_inc='jpg,png,gif,jpeg';
 				$img_inc=explode(',',$img_inc);
@@ -23,7 +21,7 @@
 					$val=explode('.',$value);
 					$endF=strtolower(end($val));
 					if(in_array($endF, $img_inc)){
-						echo "<a href='$path/$value'><img src='$path/small/$value'></a>";
+						echo "<a href='$path/$value'><img src='$path/small/$value' alt='randomSlideshow'></a>";
 						}
 				}
 
