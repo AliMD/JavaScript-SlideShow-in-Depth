c116b18bf0ae1db4aeb91ea61b72c86b7f725ade
AliGH <aghasizadeh70@gmail.com>
2012-08-21 14:35:50 +0430

    change to php and done php part

---
+++ Students Works/reflection-slider/accslide.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>JQ SLIDER</title>
+<link type="text/css" rel="stylesheet" href="../../reset.css" />
+<link type="text/css" rel="stylesheet" href="accslide.css" />
+<script type="text/javascript" src="../../zepto.js"></script>
+<script type="text/javascript" src="accslide.js"></script>
+</head>
+<body>
+<div class="container">
+	<div class="slider">
+	<br />
+		<div class="scroll">
+			<?php
+				$images_path = '../../images/reflection-slider';		
+				$images = scandir($images_path);
+				foreach($images as $img){
+					$img_arr = explode('.', $img);
+					$img_type = strtolower( end($img_arr) );
+					if ($img_type=='jpg') {
+						echo "<div><img src='$images_path/$img' width='450' height='300'></div>";
+					}
+				}
+			?>
+		</div>
+		<div class="clear"></div>
+	</div>
+</div>
+</body>
+</html>
