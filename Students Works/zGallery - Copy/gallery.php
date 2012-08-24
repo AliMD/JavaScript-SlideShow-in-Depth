fd749e237d33f3b3e48d97941f9b5e2bda4f7834
zohreh-z <zahedi.zohreh@gmail.com>
2012-08-24 17:17:49 +0430

    add text but not ok

--- Students Works/zGallery - Copy/gallery.php
+++ Students Works/zGallery - Copy/gallery.php
@@ -13,14 +13,27 @@
 		<div class="gallery">
 			<?php
 				$images_path = "../../images/zohreh-img/zgallery";
-
 				$images = scandir($images_path);
+				
 				foreach ($images as $img) {
 					$exp = explode('.', $img);
 					$img_type = strtolower(end($exp));
+					/*
+					$file_name = substr($img,0,-strlen($img_type)-1);
+
+					$desc_path = "$images_path/$file_name.txt";
+
+					if( file_exists($desc_path) ){
+						$desc_text = file_get_contents($desc_path);
+						$desc_html = "<div class='desc'>$desc_text</div>";
+					}else{
+						$desc_html = "";
+					}
+*/
+					
 					if($img_type == 'jpg')
 						echo "<div class='bg-div' style='background-image:url($images_path/$img);'>
-								<div class='text'>sdn jksdkcnbsjcns</div>
+							
 							</div>";
 				}
 			?>
