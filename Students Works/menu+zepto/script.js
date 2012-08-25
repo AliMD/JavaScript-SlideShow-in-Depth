2f5ce9c95f49abe96671b16b3b3f862bc1055686
s-soodmand <somaieh.soodmand@gmail.com>
2012-08-25 16:11:22 +0330

    menu with zepto

---
+++ Students Works/menu+zepto/script.js
@@ -0,0 +1,19 @@
+/*$(function(){
+
+	var list = $('nav.right-menu li');
+		du = 400;
+		delay = 100;
+
+	list.each(function(){
+		var that = $('this');
+		setTimeout(function(){
+			that.animate({
+				left:'-70'
+			},du);
+		},delay += du);
+	});
+
+	list.mouseover(function(){
+		$(this).animate({left:'100px'},500);
+	});
+}) */
