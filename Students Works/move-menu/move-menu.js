d4dcaa6bcfc31c08cbdc28c80fc389c4b553b312
zohreh-z <zahedi.zohreh@gmail.com>
2012-08-25 16:28:06 +0430

    CHANGE ANIMATION

--- Students Works/move-menu/move-menu.js
+++ Students Works/move-menu/move-menu.js
@@ -1,25 +1,39 @@
 // JavaScript Document
 $(function(){
 	var listmenuli = $('div.container menu.centermenu li'),
-		lf = 350,
+		lf = 300,
 		duration = 100,
-		delay = 60; 
+		delay = 150; 
+
 	listmenuli.each(function(){
 		var that = $(this);
 		setTimeout(function(){
 			that.animate({
 				left:lf,
-				top:-58
+				top:-8
 			},500);
 			lf += 105;
 		},duration+=delay);
 	});
 	
+	listmenuli.each(function(){
+		var that= $(this);
+		setTimeout(move1=function(){
+		that.animate({
+				top:-60
+			},500);
+		},duration+=delay);
+		
+	});
+
+	setInterval(move1,5000);
+		
+	listmenuli.mouseout(function(){
+		$(this).animate({top:-60},300);
+	});	
+	
 	listmenuli.mouseover(function(){
 		$(this).animate({top:-5},300);
 	});
 	
-	listmenuli.mouseout(function(){
-		$(this).animate({top:-60},300);
-	});
 });
