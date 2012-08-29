df99d61cb969b318c44b3d4d763a94b91ca765b4
mahlad <mahlad365@gmail.com>
2012-08-29 17:35:03 +0430

    add padding

--- Students Works/mahlad-PartofImg-Slideshow-02/script/partofImg.js
+++ Students Works/mahlad-PartofImg-Slideshow-02/script/partofImg.js
@@ -4,6 +4,7 @@ $(function(){
 	opacitySl=$('div.partSl div.imgSl > div > span')
 	partSl.animate({width:partWIdth+'px'},800);
 	partSl.last().css({'padding-right':'400px'});
+	opacitySl.last().css({'padding-right':'400px'});
 	partSl.mouseover(function(){
 		partSl.animate({width:'60px'},800);
 		$(this).animate({width:'500px'},800);
