6c5a83eec3d6bf137e03889f8d4bffc9cc5564a1
mahlad <mahlad365@gmail.com>
2012-08-30 12:55:20 +0430

    add a for click

--- Students Works/SlideshowRandom/index.php
+++ Students Works/SlideshowRandom/index.php
@@ -14,13 +14,13 @@
 			<?php
 				$path='../../images/SlideshowRandom';
 				$img_arr=scandir($path);
-				$img_inc='jpg,png,gif,jpeg';
+				$img_inc='jpg,jpeg';
 				$img_inc=explode(',',$img_inc);
 				foreach ($img_arr as $value) {
 					$val=explode('.',$value);
 					$endF=strtolower(end($val));
 					if(in_array($endF, $img_inc)){
-						echo "<div class='imgS'><img src='$path/small/$value'></div>";
+						echo "<div class='imgS'><a href='$path/$value' title='Baby Album'><img src='$path/small/$value'></a></div>";
 						
 						}
 				}
